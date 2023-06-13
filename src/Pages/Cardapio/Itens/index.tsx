0bbe9c38dbec4fbd68fc8a3ada5a6b8594079b40
Eduardo Martins <edu.almeida.01@hotmail.com>
2023-06-13 17:53:06 -0300

    refatorando código e escrevendo página de inicio

--- src/Pages/Cardapio/Itens/index.tsx
+++ src/Pages/Cardapio/Itens/index.tsx
@@ -1,55 +1,54 @@
+/* eslint-disable indent */
 import Item from './Item';
-import cardapio from './itens.json';
+import cardapio from 'data/cardapio.json';
 import styles from './Itens.module.scss';
-import { useState, useEffect }  from 'react';
+import { useState, useEffect } from 'react';
 
 interface Props {
-    busca: string,
-    filtro: number | null,
-    ordenador:string
+  busca: string;
+  filtro: number | null;
+  ordenador: string;
 }
 
-export default function Itens( props: Props ){
-  const [ lista, setLista ] = useState(cardapio);
+export default function Itens(props: Props) {
+  const [lista, setLista] = useState(cardapio);
   const { busca, filtro, ordenador } = props;
 
-    
-  useEffect(() => { 
-    function testaBusca( title : string ){
-      const regex = new RegExp(busca, 'i');
-      return regex.test( title);
-    }
-    
-    function testaFiltro( id : number ){
-      if(filtro !== null) return filtro === id;
-      return true;
-    }
+  function testaBusca(title: string) {
+    const regex = new RegExp(busca, 'i');
+    return regex.test(title);
+  }
 
+  function testaFiltro(id: number) {
+    if (filtro !== null) return filtro === id;
+    return true;
+  }
+
+  useEffect(() => {
     const ordenar = (novaLista: typeof cardapio) => {
       switch (ordenador) {
-      case 'porcao':
-        return novaLista.sort((a, b) => (a.size > b.size ? 1 : -1));
-      case 'qtd_pessoas':
-        return novaLista.sort((a, b) => (a.serving > b.serving ? 1 : -1));
-      case 'preco':
-        return novaLista.sort((a, b) => (a.price > b.price ? 1 : -1));
-      default:
-        return novaLista;
+        case 'porcao':
+          return novaLista.sort((a, b) => (a.size > b.size ? 1 : -1));
+        case 'qtd_pessoas':
+          return novaLista.sort((a, b) => (a.serving > b.serving ? 1 : -1));
+        case 'preco':
+          return novaLista.sort((a, b) => (a.price > b.price ? 1 : -1));
+        default:
+          return novaLista;
       }
     };
 
-    const novaLista = cardapio.filter(item => testaBusca(item.title) && 
-        testaFiltro( item.category.id ));
-    setLista( ordenar(novaLista) );
-  }, [ busca, filtro, ordenador ]);
+    const novaLista = cardapio.filter(
+      (item) => testaBusca(item.title) && testaFiltro(item.category.id)
+    );
+    setLista(ordenar(novaLista));
+  }, [busca, filtro, ordenador]);
 
-  return(
-    <div className={ styles.itens }>
-      {lista.map(item => (
-        <div key={item.id} >
-          <Item 
-            {...item}
-          />
+  return (
+    <div className={styles.itens}>
+      {lista.map((item) => (
+        <div key={item.id}>
+          <Item {...item} />
         </div>
       ))}
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import 'normalize.css';
 import './index.css';
 import Cardapio from './Pages/Cardapio';
-
+import Inicio from 'Pages/Inicio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Cardapio/>
+    <Inicio />
   </React.StrictMode>
 );
-
