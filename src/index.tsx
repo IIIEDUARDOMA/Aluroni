6fd78e968871df749ce8cb6f816cd14fb2bd3c2a
Eduardo Martins <edu.almeida.01@hotmail.com>
2023-06-12 21:22:39 -0300

    adicionando filtros e imagens. Finalizando projeto

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css';
 import './index.css';
+import Cardapio from './Pages/Cardapio';
 
 
 const root = ReactDOM.createRoot(
@@ -8,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div>Aluroni</div>
+    <Cardapio/>
   </React.StrictMode>
 );
 
