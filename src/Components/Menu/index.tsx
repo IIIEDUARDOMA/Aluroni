448ea3b238934c9670f38a915822220dfadbfc07
Eduardo Martins <edu.almeida.01@hotmail.com>
2023-06-13 19:40:17 -0300

    trocando tag a por Link no Menu

--- src/Components/Menu/index.tsx
+++ src/Components/Menu/index.tsx
@@ -1,5 +1,6 @@
 import styles from './Menu.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import { Link } from 'react-router-dom';
 
 export default function Menu() {
   const rotas = [
@@ -22,7 +23,7 @@ export default function Menu() {
       <ul className={styles.menu__list}>
         {rotas.map((rota, index) => (
           <li key={index} className={styles.menu__link}>
-            <a href={rota.to}>{rota.label}</a>
+            <Link to={rota.to}>{rota.label}</Link>
           </li>
         ))}
       </ul>
