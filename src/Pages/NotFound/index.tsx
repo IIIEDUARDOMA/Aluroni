e5b78245330628af8fe80c6fdcd3f28abf15ff81
Eduardo Martins <edu.almeida.01@hotmail.com>
2023-06-13 22:11:50 -0300

    criando página de NotFound

---
+++ src/Pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import styles from './NotFound.module.scss';
+import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
+import classNames from 'classnames';
+import stylesTema from 'styles/Temas.module.scss';
+
+export default function NotFound() {
+  return (
+    <div
+      className={classNames({
+        [styles.container]: true,
+        [stylesTema.container]: true,
+      })}
+    >
+      <NotFoundImage />
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,27 @@
+import Footer from 'Components/Footer';
 import Menu from 'Components/Menu';
 import PaginaPadrao from 'Components/PaginaPadrao';
 import Cardapio from 'Pages/Cardapio';
 import Inicio from 'Pages/Inicio';
+import NotFound from 'Pages/NotFound';
 import Sobre from 'Pages/Sobre';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Menu />
-      <Routes>
-        <Route path="/" element={<PaginaPadrao />}>
-          <Route index element={<Inicio />} />
-          <Route path="cardapio" element={<Cardapio />} />
-          <Route path="sobre" element={<Sobre />} />
-        </Route>
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+            <Route path="sobre" element={<Sobre />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </main>
   );
 }
